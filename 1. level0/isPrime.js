bcbc058a8e085723894d14655ddf3e2aad4e5a80
Itz_me_Hazel <deaese94@gmail.com>
2023-02-20 10:18:17 +0900

    2023-02-20 알고리즘 풀이

---
+++ 1. level0/isPrime.js
@@ -0,0 +1,24 @@
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(nums) {
+  let answer = 0;
+  const n = nums.length;
+
+  // 3개의 수를 선택하는 3중 반복문
+  for (let i = 0; i < n - 2; i++) {
+    for (let j = i + 1; j < n - 1; j++) {
+      for (let k = j + 1; k < n; k++) {
+        const sum = nums[i] + nums[j] + nums[k];
+        if (isPrime(sum)) answer++;
+      }
+    }
+  }
+
+  return answer;
+}
